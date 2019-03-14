147162d2791109b9f8520722398c2250a26100e4
svcarpacomp <svcarpacomp@arpacomp.pnl.gov>
2019-03-14 13:12:33 -0700

    gurobi test

--- MyJulia1.jl
+++ MyJulia1.jl
@@ -1,7 +1,6 @@
+using JuMP
 
 using Pkg
-Pkg.activate(".")
-Pkg.instantiate()
 Pkg.status()
 
 using Ipopt
@@ -19,4 +18,35 @@ function MyJulia1(InFile1::String, InFile2::String, InFile3::String, InFile4::St
     println("  $(TimeLimitInSeconds)")
     println("  $(ScoringMethod)")
     println("  $(NetworkModel)")
+
+
+m = Model(solver=Gurobisolver())
+@variable(m, x >=0)
+@variable(m, y >=0)
+
+@variable(m, x >= LB)
+@variable(m, x <= UB)
+@variable(m, LB  <=  x  <= UB)
+
+@variable(m, x >=0, Int)
+@variable(m, x >=0, Bin)
+
+@variable(m, x[1:M,1:N]  >= 0)
+
+@variable(m, x[i=1:5], start=(i))
+
+@objective(m, Min, 10x + 26y)
+
+@constraint(m, const1,  11x + 3y >=  21)
+@constraint(m, const2,   6x + 20y >= 39)
+
+status = solve(m)
+println("Status = $status")
+
+println("Optimal Objective Function value: ", getObjectiveValue(m))
+println("Optimal Solutions:")
+println("x = ", getValue(x))
+println("y = ", getValue(y))
+
+
 end
